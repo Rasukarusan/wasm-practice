00fa02bb8ba39103a82be5319821b36eeb9a5abe
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-05-28 11:24:55 +0900

    wasmでhello world

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "syscall/js"
+
+func main() {
+	ch := make(chan struct{})
+	js.Global().Get("document").Call("getElementById", "hello").Set("innerHTML", "Hello, World!")
+	<-ch // Code must not finish
+}
